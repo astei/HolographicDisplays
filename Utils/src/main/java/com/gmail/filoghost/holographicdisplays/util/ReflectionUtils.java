81386fd1a22edaaf9fac82310f4b27b2a5a23cd6
Andrew Steinborn <git@steinborn.me>
2018-07-22 09:50:46 -0400

    Totally patch out getStackTraceElement() method
    This method doesn't work on Java 9+ and the cross-JVM compatible
    version is just as expensive as taking a full stack trace. Now add
    adding getId() many, many times, which takes a stack trace every time,
    and you've got a recipe for performance problems.

--- Utils/src/main/java/com/gmail/filoghost/holographicdisplays/util/ReflectionUtils.java
+++ Utils/src/main/java/com/gmail/filoghost/holographicdisplays/util/ReflectionUtils.java
@@ -28,41 +28,9 @@ public class ReflectionUtils {
 		field.setAccessible(true);
 		return field.get(handle);
 	}
-	
-	private static Method getStackTraceElementMethod;
-	private static Method getStackTraceDepthMethod;
-	
-	private static boolean stackTraceErrorPrinted;
-	
-	/**
-	 * If you only need one stack trace element this is faster than Throwable.getStackTrace()[element],
-	 * it doesn't generate the full stack trace.
-	 */
+
 	public static StackTraceElement getStackTraceElement(int index) {
-		try {
-			if (getStackTraceElementMethod == null) {
-				getStackTraceElementMethod = Throwable.class.getDeclaredMethod("getStackTraceElement", int.class);
-				getStackTraceElementMethod.setAccessible(true);
-			}
-			if (getStackTraceDepthMethod == null) {
-				getStackTraceDepthMethod = Throwable.class.getDeclaredMethod("getStackTraceDepth");
-				getStackTraceDepthMethod.setAccessible(true);
-			}
-			
-			Throwable dummyThrowable = new Throwable();
-			int depth = (Integer) getStackTraceDepthMethod.invoke(dummyThrowable);
-			
-			if (index < depth) {
-				return (StackTraceElement) getStackTraceElementMethod.invoke(new Throwable(), index);
-			} else {
-				return null;
-			}
-		} catch (Throwable t) {
-			if (!stackTraceErrorPrinted) {
-				Bukkit.getPluginManager().getPlugin("HolographicDisplays").getLogger().log(Level.WARNING, "Unable to get a stacktrace element, please inform the developer. You will only see this error once to avoid spam.", t);
-				stackTraceErrorPrinted = true;
-			}
-			return null;
-		}
+		// Patched out: taking stack traces every time you call getId() is extremely expensive with lots of holograms.
+		return null;
 	}
 }
